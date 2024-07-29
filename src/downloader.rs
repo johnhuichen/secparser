8aefb7714b3aac8f43ffc8496a31370ce024ca93
John Chen <johnhuichen@gmail.com>
2024-07-29 15:04:13 -0400

    Add interactive ui

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,43 +1,29 @@
 use colored::Colorize;
 use core::panic;
-use reqwest::header::{ACCEPT_ENCODING, HOST, USER_AGENT};
-use reqwest::{self, Url};
+use inquire::{validator::Validation, Confirm, Text};
+use reqwest::{
+    self,
+    header::{ACCEPT_ENCODING, HOST, USER_AGENT},
+    Url,
+};
 use serde::{Deserialize, Serialize};
-use std::error::Error;
 use std::fs::{self, File};
 use std::io::{copy, Write};
 use std::path::{Path, PathBuf};
-
-use crate::prompt;
+use std::{error::Error, thread::sleep, time::Duration};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct DownloadConfig {
-    #[serde(default = "default_user_agent")]
     user_agent: String,
-
-    #[serde(default = "default_temp_dir")]
-    temp_dir: String,
-}
-
-fn default_user_agent() -> String {
-    "example@domain.com".to_string()
-}
-
-fn default_temp_dir() -> String {
-    "tmp".to_string()
 }
 
 impl DownloadConfig {
     pub fn new() -> Self {
-        let filepath = "config/download.local.toml";
+        let filepath = "config/.download.toml";
 
         Self::create_file_if_missing(filepath);
 
-        let cfg = Self::parse_cfg_from_toml(filepath);
-
-        Self::panic_if_invalid_cfg(filepath, &cfg);
-
-        cfg
+        Self::parse_cfg_from_toml(filepath)
     }
 
     fn create_file_if_missing(filepath: &str) {
@@ -51,33 +37,44 @@ impl DownloadConfig {
         let contents = fs::read_to_string(filepath)
             .unwrap_or_else(|_| panic!("Should open config {filepath}"));
 
-        let cfg: DownloadConfig = toml::from_str(&contents)
-            .unwrap_or_else(|e| panic!("Should parse config {filepath} {e}"));
-
-        if contents.is_empty() {
-            let cfg_content = toml::to_string(&cfg).unwrap();
-            let mut file = File::create(filepath).unwrap_or_else(|_| panic!());
-            file.write_all(cfg_content.as_bytes())
-                .unwrap_or_else(|_| panic!("Should write default config to empty config file"));
+        if !contents.is_empty() {
+            return toml::from_str(&contents)
+                .unwrap_or_else(|e| panic!("Should parse config {filepath} {e}"));
         }
+
+        let user_agent = Self::get_user_input("Enter an email address as user agent (see https://www.sec.gov/search-filings/edgar-search-assistance/accessing-edgar-data for reasons)");
+        let cfg = DownloadConfig { user_agent };
+        let cfg_content = toml::to_string(&cfg).unwrap();
+        let mut file = File::create(filepath).unwrap_or_else(|_| panic!());
+        file.write_all(cfg_content.as_bytes())
+            .unwrap_or_else(|_| panic!("Should write default config to empty config file"));
 
         cfg
     }
 
-    fn panic_if_invalid_cfg(filepath: &str, cfg: &Self) {
-        if cfg.user_agent == default_user_agent() {
-            panic!(
-                "Default user agent {} is used. You have to update it in {}",
-                cfg.user_agent.to_string().bright_red(),
-                filepath.to_string().bright_blue()
-            );
-        }
+    fn get_user_input(msg: &str) -> String {
+        let validator = |input: &str| {
+            if input.chars().count() == 0 {
+                Ok(Validation::Invalid("Empty input is not allowed".into()))
+            } else {
+                Ok(Validation::Valid)
+            }
+        };
+
+        Text::new(msg)
+            .with_validator(validator)
+            .prompt()
+            .unwrap_or_else(|e| panic!("Should get user input, {e}"))
     }
 }
 
+const TEMP_DIR: &str = "tmp";
+
 pub async fn download(url: &str) -> Result<PathBuf, Box<dyn Error>> {
     let config = DownloadConfig::new();
-    let filepath = get_filepath(url, &config);
+    log::info!("Config loaded: {config:#?}");
+
+    let filepath = get_filepath(url);
 
     if should_download(&filepath) {
         download_and_save(url, &filepath, &config).await?;
@@ -86,24 +83,31 @@ pub async fn download(url: &str) -> Result<PathBuf, Box<dyn Error>> {
     Ok(filepath)
 }
 
-fn get_filepath(url: &str, config: &DownloadConfig) -> PathBuf {
-    fs::create_dir_all(&config.temp_dir)
-        .unwrap_or_else(|_| panic!("Should create directory: {}", &config.temp_dir));
+fn get_filepath(url: &str) -> PathBuf {
+    fs::create_dir_all(TEMP_DIR)
+        .unwrap_or_else(|_| panic!("Should create directory: {}", TEMP_DIR));
     let parsed_url = Url::parse(url).unwrap_or_else(|_| panic!("Should parse url: {url}"));
     let filename = parsed_url
         .path_segments()
         .and_then(|segments| segments.last())
         .unwrap_or("tmp.bin");
 
-    Path::new(&config.temp_dir).join(filename)
+    Path::new(TEMP_DIR).join(filename)
 }
 
 fn should_download(filepath: &Path) -> bool {
-    !filepath.exists()
-        || prompt::confirm(&format!(
-            "{} exists, do you want to download it again?",
-            filepath.to_string_lossy().bright_blue()
-        ))
+    if !filepath.exists() {
+        return true;
+    }
+
+    let ans = Confirm::new(&format!(
+        "File exists as {}, do you want to download it again?",
+        filepath.to_string_lossy().bright_blue(),
+    ))
+    .with_help_message("Download if you are not sure of the file conntent")
+    .prompt();
+
+    ans.unwrap()
 }
 
 async fn download_and_save(
@@ -111,11 +115,16 @@ async fn download_and_save(
     filepath: &PathBuf,
     config: &DownloadConfig,
 ) -> Result<(), Box<dyn Error>> {
-    log::info!("downloading {url} and save as {filepath:#?}");
+    log::info!("Downloading {url} and save as {filepath:#?}");
     let client = reqwest::Client::new();
+    // SEC allows max concurrent request of 10 requests/second.
+    // This is the easiest way to ensure this restriction is followed
+    // Further optimization may be needed if this becomes a performance bottleneck
+    let sleep_duration = Duration::from_millis(100);
+    sleep(sleep_duration);
     let response = client
         .get(url)
-        .header(USER_AGENT, config.user_agent.to_string())
+        .header(USER_AGENT, config.user_agent.to_string()) // see https://www.sec.gov/search-filings/edgar-search-assistance/accessing-edgar-data for reasons
         .header(ACCEPT_ENCODING, "gzip,deflate".to_string())
         .header(HOST, "www.sec.gov".to_string())
         .send()
@@ -124,6 +133,5 @@ async fn download_and_save(
     let content = response.text().await?;
     copy(&mut content.as_bytes(), &mut dest)
         .unwrap_or_else(|_| panic!("Should copy download content to file"));
-
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,67 @@
+use colored::Colorize;
+use inquire::Select;
 use std::error::Error;
 
 mod downloader;
 mod logger;
-mod prompt;
+mod parse_cik;
+
+#[derive(Debug)]
+enum MenuOptions {
+    CIK,
+    FinancialStatement,
+    Form13f,
+    Exit,
+}
+
+impl MenuOptions {
+    const CIK_DESC: &'static str = "Download and parse CIK list";
+    const FINANCIAL_STATEMENT_DESC: &'static str = "Download and parse Financial Statements";
+    const FORM13F_DESC: &'static str = "Download and parse Form 13F";
+
+    const EXIT_DESC: &'static str = "Exit";
+
+    fn from(s: &str) -> Self {
+        match s {
+            Self::CIK_DESC => Self::CIK,
+            Self::FINANCIAL_STATEMENT_DESC => Self::FinancialStatement,
+            Self::FORM13F_DESC => Self::Form13f,
+            Self::EXIT_DESC => Self::Exit,
+            _ => panic!("Should be one of menu options"),
+        }
+    }
+
+    fn as_str(&self) -> &'static str {
+        match self {
+            Self::CIK => Self::CIK_DESC,
+            Self::FinancialStatement => Self::FINANCIAL_STATEMENT_DESC,
+            Self::Form13f => Self::FORM13F_DESC,
+            Self::Exit => Self::EXIT_DESC,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     logger::init()?;
-    let url = "https://www.sec.gov/Archives/edgar/cik-lookup-data.txt";
-    let result = downloader::download(url).await?;
-    println!("{:#?}", result);
 
-    Ok(())
+    let cik = MenuOptions::CIK.as_str();
+    let financial_statement = MenuOptions::FinancialStatement.as_str();
+    let form13f = MenuOptions::Form13f.as_str();
+    let exit = MenuOptions::Exit.as_str();
+
+    let options: Vec<&str> = vec![cik, financial_statement, form13f, exit];
+
+    loop {
+        let ans = Select::new("Choose one of the options", options.clone())
+            .prompt()
+            .unwrap_or_else(|_| panic!("Should get a valid option"));
+
+        match MenuOptions::from(ans) {
+            MenuOptions::CIK => parse_cik::download_and_parse().await?,
+            MenuOptions::FinancialStatement => println!("{}", "Not implemented!".bright_red()),
+            MenuOptions::Form13f => println!("{}", "Not implemented!".bright_red()),
+            MenuOptions::Exit => return Ok(()),
+        }
+    }
 }
