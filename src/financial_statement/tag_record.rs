4ca0fac03efc38ed417a8cca1d6cf1cea56b659d
John Chen <johnhuichen@gmail.com>
2024-08-09 21:56:33 -0400

    Tag parsing all correct

--- src/financial_statement/tag_record.rs
+++ src/financial_statement/tag_record.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 use crate::deserializer::bool_from_int;
 
 use super::data_source::FsDataSource;
-use super::record::{FileIter, FsRecords, MaybeRecordIter};
+use super::record::{FsRecords, FsRecordsConfig, FsRecordsIters, MaybeRecordIter};
 
 #[derive(Debug, Deserialize)]
 pub struct FsTag {
@@ -22,35 +22,37 @@ pub struct FsTag {
 }
 
 pub struct FsTagRecords {
-    file_iter: FileIter,
-    maybe_record_iter: MaybeRecordIter<FsTag>,
+    iters: FsRecordsIters<FsTag>,
+    config: FsRecordsConfig,
 }
 
 impl FsRecords<FsTag> for FsTagRecords {
-    fn get_file_iter_field(&mut self) -> &mut FileIter {
-        &mut self.file_iter
+    fn get_iters(&mut self) -> &mut FsRecordsIters<FsTag> {
+        &mut self.iters
     }
 
-    fn get_maybe_record_iter_field(&mut self) -> &mut MaybeRecordIter<FsTag> {
-        &mut self.maybe_record_iter
+    fn update_iters(&mut self, maybe_record_iter: MaybeRecordIter<FsTag>) {
+        self.iters.maybe_record_iter = maybe_record_iter
     }
 
-    fn update_maybe_record_iter(&mut self, maybe_record_iter: MaybeRecordIter<FsTag>) {
-        self.maybe_record_iter = maybe_record_iter;
+    fn get_config(&self) -> &FsRecordsConfig {
+        &self.config
     }
 }
 
 impl FsTagRecords {
     const TSV_FILENAME: &'static str = "tag.tsv";
 
-    pub fn new(data_source: FsDataSource) -> Result<Self> {
+    pub fn new(data_source: FsDataSource, config: FsRecordsConfig) -> Result<Self> {
         let mut file_iter = data_source.filepaths.into_iter();
-        let maybe_record_iter = Self::get_maybe_record_iter(&mut file_iter, Self::TSV_FILENAME)?;
-
-        Ok(Self {
+        let maybe_record_iter =
+            Self::get_maybe_record_iter(config.clone(), &mut file_iter, Self::TSV_FILENAME)?;
+        let iters = FsRecordsIters {
             file_iter,
             maybe_record_iter,
-        })
+        };
+
+        Ok(Self { iters, config })
     }
 }
 
@@ -59,5 +61,40 @@ impl Iterator for FsTagRecords {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.do_next(Self::TSV_FILENAME)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::downloader::DownloadConfigBuilder;
+    use crate::traits::DataSource;
+    use anyhow::Result;
+    use chrono::NaiveDate;
+
+    use super::*;
+
+    #[test]
+    fn it_parses_fs_tag() -> Result<()> {
+        env_logger::init();
+
+        let user_agent = "example@secparser.com".to_string();
+        let download_config = DownloadConfigBuilder::default()
+            .user_agent(user_agent)
+            .download_dir("./download".to_string())
+            .build()?;
+
+        let from_date = NaiveDate::from_ymd_opt(2009, 1, 1).unwrap();
+        let data_source = FsDataSource::new(&download_config, from_date)?;
+        data_source.validate_cache()?;
+        log::info!("Data source cache is validated");
+
+        let record_config = FsRecordsConfig { strict_mode: true };
+        let records = FsTagRecords::new(data_source, record_config)?;
+
+        for record in records {
+            log::info!("{:?}", record);
+        }
+
+        Ok(())
     }
 }
